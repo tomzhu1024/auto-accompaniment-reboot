ac3fec2ef8b674f7fdd57040794a89ac180bae97
Liyang Zhu <1184997399@qq.com>
2021-03-02 11:24:44 +0800

    updated install scripts

--- install-core.sh
+++ install-core.sh
@@ -3,6 +3,6 @@ pip install python-rtmidi
 pip install -e git://github.com/nwhitehead/pyfluidsynth.git#egg=pyFluidSynth
 conda install -c anaconda numpy -y
 conda install -c anaconda statsmodels -y
-conda install -c anaconda pyaudio
+conda install -c anaconda pyaudio -y
 conda install -c conda-forge matplotlib -y
 conda install -c conda-forge aubio -y
